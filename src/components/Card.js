b8834031402765bf163b728ab1ca5a06d8f1106f
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-08-08 23:31:25 -0500

    Adding fontawesome and styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,16 @@ const Card = ({character}) => {
                 <img src={character.image}/>
                 <div className="card-body">
                     <h6>{character.name}</h6>
-                    <p>{character.species}</p>
+                    <small className="m-0 text-small">{character.species == 'Human' 
+                        ? <div><i class="fa-solid fa-user mr-2"></i> Human</div>
+                        : <div><i class="fa-brands fa-reddit-alien mr-2"></i> Alien</div>}
+                    </small>
+                    <small className="m-0">{character.status == 'Alive'
+                        ? <div><i class="fa-solid fa-heart mr-2"></i> Vivo</div>
+                        : character.status == 'Dead'
+                        ? <div><i class="fa-solid fa-person-falling-burst mr-2"></i> Muerto</div>
+                        : <div><i class="fa-solid fa-question mr-2"></i> Desconocido</div>}
+                    </small>
                 </div>
             </div>
         </div>
